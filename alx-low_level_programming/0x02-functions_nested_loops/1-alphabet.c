7bf9fe8c053512bbfb37b46c55f9435afd006d8d
Kamogelo23 <mahomakamogelo.com>
2023-09-12 20:31:09 -0700

    New

---
+++ alx-low_level_programming/0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet - wirte the the lower-case alphabet on a line.
+ *
+ * Return:void
+ */
+void print_alphabet(void)
+{
+char ch;
+for (c = 'a' ; ch <= 'z' ; ch++)
+_putchar(ch);
+_putchar('\n');
+}
+
