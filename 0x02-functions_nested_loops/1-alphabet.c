7baca07b87acf586643d77990f8e0c88a08832de
Mahoma Kamogelo <75945527+Kamogelo23@users.noreply.github.com>
2023-09-14 16:44:00 +0200

    Add files via upload

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - wirte the the lower-case alphabet on a line.
+ *
+ * Return:void
+ */
+
+void print_alphabet(void)
+{
+char ch;
+for (ch  = 'a'; ch <= 'z'; ch++)
+_putchar(ch);
+_putchar('\n');
+}
+
